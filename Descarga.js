f3e032e541fa60abf463ef587fa300533ef88ddc
Nachodusan <105814831+Nachodusan@users.noreply.github.com>
2024-11-16 07:16:54 -0300

    Add files via upload

---
+++ Descarga.js
@@ -0,0 +1,4 @@
+// Función para activar la descarga al hacer clic en el botón
+document.getElementById('btnDescargar').addEventListener('click', function() {
+    document.getElementById('descargaEnlace').click();
+});
